7b358518e235bdc21bae4fa497adfdafc70b13fb
keertanaganiga <keertanaganiga@gmail.com>
2022-05-12 13:35:16 +0530

    Recycler view

--- app/src/main/java/com/keertana/sampleswag/Controller/MainActivity.kt
+++ app/src/main/java/com/keertana/sampleswag/Controller/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.keertana.sampleswag.Controller
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.keertana.sampleswag.Adapters.CategoryAdapter
 import com.keertana.sampleswag.R
@@ -17,5 +18,10 @@ class MainActivity : AppCompatActivity() {
 
         adapter= CategoryAdapter(this,DataService.categories)
         categoryListView.adapter=adapter
+
+//        categoryListView.setOnItemClickListener { adapterView, view, i, id ->
+//            val category=DataService.categories[i]
+//            Toast.makeText(this,"You clicked on the ${category.title} cell",Toast.LENGTH_SHORT).show()
+        }
     }
 }
