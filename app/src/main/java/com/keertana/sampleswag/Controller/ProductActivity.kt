147578ea47311a8f469944de63d04c05897f2361
keertanaganiga <keertanaganiga@gmail.com>
2022-05-19 14:46:07 +0530

    final part

--- app/src/main/java/com/keertana/sampleswag/Controller/ProductActivity.kt
+++ app/src/main/java/com/keertana/sampleswag/Controller/ProductActivity.kt
@@ -1,16 +1,39 @@
 package com.keertana.sampleswag.Controller
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import com.keertana.sampleswag.Adapters.ProductsAdapter
 import com.keertana.sampleswag.R
+import com.keertana.sampleswag.Services.DataService
 import com.keertana.sampleswag.Utilities.EXTRA_CATEGORY
+import kotlinx.android.synthetic.main.activity_product.*
 
 class ProductActivity : AppCompatActivity() {
+
+    lateinit var adapter:ProductsAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product)
 
         val categoryType=intent.getStringExtra(EXTRA_CATEGORY)
-        println(categoryType)
+        adapter= ProductsAdapter(this,DataService.getProducts(categoryType))
+
+        var spanCount=2
+        val orientation=resources.configuration.orientation
+        if (orientation==Configuration.ORIENTATION_LANDSCAPE){
+            spanCount=3
+        }
+
+        var screenSize=resources.configuration.screenWidthDp
+        if (screenSize>720){
+            spanCount=3
+        }
+
+        val layoutManager=GridLayoutManager(this,spanCount)
+        productsListView.layoutManager=layoutManager
+        productsListView.adapter=adapter
     }
 }
--- app/src/main/java/com/keertana/sampleswag/Services/DataService.kt
+++ app/src/main/java/com/keertana/sampleswag/Services/DataService.kt
@@ -16,27 +16,53 @@ object DataService {
         Product("Sample Hat Black","$21","hat1"),
         Product("Sample Hat Purple","$18","hat2"),
         Product("Sample Hat White","$23","hat3"),
-        Product("Sample Hat Blue","$16","hat4")
+        Product("Sample Hat Blue","$16","hat4"),
+            Product("Sample Hat Black","$21","hat1"),
+            Product("Sample Hat Purple","$18","hat2"),
+            Product("Sample Hat White","$23","hat3"),
+            Product("Sample Hat Blue","$16","hat4"),
+            Product("Sample Hat Black","$21","hat1"),
+            Product("Sample Hat Purple","$18","hat2"),
+            Product("Sample Hat White","$23","hat3"),
+            Product("Sample Hat Blue","$16","hat4")
     )
 
     val hoodies= listOf(
         Product("Sample Hoodie Black","$25","hoodie1"),
         Product("Sample Hoodie Purple","$21","hoodie2"),
         Product("Sample Hoodie White","$35","hoodie3"),
-        Product("Sample Hoodie Blue","$40","hoodie4")
+        Product("Sample Hoodie Blue","$40","hoodie4"),
+            Product("Sample Hoodie Black","$25","hoodie1"),
+            Product("Sample Hoodie Purple","$21","hoodie2"),
+            Product("Sample Hoodie White","$35","hoodie3"),
+            Product("Sample Hoodie Blue","$40","hoodie4"),
+            Product("Sample Hoodie Black","$25","hoodie1"),
+            Product("Sample Hoodie Purple","$21","hoodie2"),
+            Product("Sample Hoodie White","$35","hoodie3"),
+            Product("Sample Hoodie Blue","$40","hoodie4")
     )
 
     val shirts= listOf(
         Product("Sample Shirt Black","$45","shirt1"),
         Product("Sample Shirt Purple","$35","shirt2"),
         Product("Sample Shirt White","$25","shirt3"),
-        Product("Sample Shirt Blue","$20","shirt0"),
+        Product("Sample Shirt Blue","$20","shirt4"),
         Product("Sample Shirt Pink","$50","shirt5"),
+            Product("Sample Shirt Black","$45","shirt1"),
+            Product("Sample Shirt Purple","$35","shirt2"),
+            Product("Sample Shirt White","$25","shirt3"),
+            Product("Sample Shirt Blue","$20","shirt4"),
+            Product("Sample Shirt Pink","$50","shirt5"),
+            Product("Sample Shirt Black","$45","shirt1"),
+            Product("Sample Shirt Purple","$35","shirt2"),
+            Product("Sample Shirt White","$25","shirt3"),
+            Product("Sample Shirt Blue","$20","shirt4"),
+            Product("Sample Shirt Pink","$50","shirt5")
     )
 
     val digitalGood= listOf<Product>()
 
-    fun getProducts(category:String):List<Product>{
+    fun getProducts(category: String?):List<Product>{
         return when(category){
             "SHIRTS" -> shirts
             "HATS" -> hats
@@ -44,6 +70,6 @@ object DataService {
             else -> digitalGood
         }
 
-        
+
     }
 }
